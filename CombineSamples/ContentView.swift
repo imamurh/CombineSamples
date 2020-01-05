c670f293a679a84fb1c64e8a616a3966f84c298b
imamurh <imamurh@gmail.com>
2020-01-06 04:05:15 +0900

    Initial Commit

---
+++ CombineSamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineSamples
+//
+//  Created by Hajime Imamura on 2020/01/06.
+//  Copyright © 2020 imamurh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
